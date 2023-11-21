463be0c4c727fc063ed847dd1d6e32382b1b1415
Reigniel Cabela <148176276+Reigncbl@users.noreply.github.com>
2023-11-21 11:35:59 +0800

    create day 1 output.py

---
+++ backend/day-01/day-1-output.py
@@ -0,0 +1,5 @@
+name = "Reigniel C. Cabela"
+name_of_department = "DSWD"
+about_me = "I'm a passionate college student pursuing a degree in IT."
+
+print(f"Hello, I am {name}. I love {name_of_department} and I love being in the Backend even more!\nOne thing about me is that {about_me}")
