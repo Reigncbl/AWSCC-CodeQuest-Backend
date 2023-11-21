23e3583cc1e694b44ddca84c5ed9f4da289e7bb7
Reigniel Cabela <148176276+Reigncbl@users.noreply.github.com>
2023-11-21 12:44:47 +0800

    Create day-2-output.py

---
+++ backend/day-02/day-2-output.py
@@ -0,0 +1,10 @@
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+third = input("Enter the third number: ")
+
+first = int(first)
+second = int(second)
+third = int(third)
+
+sum = first + second + third
+print(f"sum: {sum}")
